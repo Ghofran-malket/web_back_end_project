34e6a38025bd676645359d36dab8dd245389a8ec
Ghofran <gh.malket@gmail.com>
2024-07-22 23:06:53 +0200

    Ajouter notre selection page avec sa contoller

--- src/Controller/NotreSelectionController.php
+++ src/Controller/NotreSelectionController.php
@@ -6,21 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class NotreSelectionController extends AbstractController
 {
     public function __construct(private ProductController $productController) {
     }
 
-    #[Route('/home', name: 'gephora_home')]
+    
+    #[Route('/notre_selection', name: 'gephora_notre_selection')]
     public function index(): Response
     {
         $products_notre_selection = $this->productController->showByCategory('Notre Selection');
-        $products_tendane = $this->productController->showByCategory('Tendance');
-        $products_offre = $this->productController->showByCategory('Offre');
-        return $this->render('pages/home.html.twig', [
+        return $this->render('pages/notre_selection.html.twig', [
             'products_notre_selection' => $products_notre_selection,
-            'products_tendane' => $products_tendane,
-            'products_offre' => $products_offre
         ]);
     }
 }
